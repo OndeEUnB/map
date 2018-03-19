a4f9adb02dc4c55f3dd412fa9558348060d5e54d
Kairon Velozo <kairon333@gmail.com>
2018-03-19 15:08:06 -0300

    Add mapbox

--- src/helpers/map.js
+++ src/helpers/map.js
@@ -1,9 +1,7 @@
-import { MAP_ATTRIBUTION, MAP_OPTIONS, MAP_TILES } from '../constants/map';
-import { map, tileLayer } from 'leaflet';
+import mapboxgl from 'mapbox-gl';
+import { TILES_TOKEN, MAP_OPTIONS } from '../constants/map';
 
 export const buildMap = () => {
-  const tiles = tileLayer(MAP_TILES, MAP_ATTRIBUTION);
-  const myMap = map('map', MAP_OPTIONS);
-  tiles.addTo(myMap);
-  return myMap;
+  mapboxgl.accessToken = TILES_TOKEN;
+  return new mapboxgl.Map(MAP_OPTIONS);
 };
